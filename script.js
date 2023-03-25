a7055786e77c3d53135b5f5b7306157ea457d566
Remek-P <remek.piotrowski@wp.pl>
2023-03-25 19:28:28 +0100

    Remove single list item Remove all list items show/hide filter and clear button when there are no items Filtering items

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const itemForm = document.getElementById("item-form");
-const itemList = document.getElementById("item-list");
-const itemInput = document.getElementById("item-input");
+const itemForm    = document.getElementById("item-form");
+const itemInput   = document.getElementById("item-input");
+const itemList    = document.getElementById("item-list");
+const buttonClear = document.getElementById("clear");
+const itemFilter  = document.getElementById("filter");
 
 const createIcon = (classes) => {
   const icon = document.createElement("i");
   icon.className = classes;
-  return icon
+  return icon;
 }
 
 const createButton = (classes) => {
@@ -13,7 +15,7 @@ const createButton = (classes) => {
   button.className = classes;
   const icon = createIcon("fa-solid fa-xmark");
   button.appendChild(icon);
-  return button
+  return button;
 }
 
 const addItem = (e) => {
@@ -37,9 +39,67 @@ const addItem = (e) => {
   const button = createButton("remove-item btn-link text-red")
   li.appendChild(button)
 
+  // Adding li
   itemList.appendChild(li);
+
+  changeUIWhenNoLi();
 
   itemInput.value = "";
 }
 
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?"))
+    e.target.parentElement.parentElement.remove();
+
+    changeUIWhenNoLi();
+  }
+}
+
+const clearAllItems = () => {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild)
+  }
+  changeUIWhenNoLi();
+}
+
+const changeUIWhenNoLi = () => {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    buttonClear.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    buttonClear.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+};
+
+const filterItems = (e) => {
+  const text  = e.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach(item => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    // Show/hide filtered item list
+    if (itemName.indexOf(text) !== -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+    // Show/hide clear all button
+    if (text) {
+      buttonClear.style.display = "none";
+    } else {
+      buttonClear.style.display = "block";
+    }
+  })
+};
+
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+buttonClear.addEventListener("click", clearAllItems);
+itemFilter.addEventListener("input", filterItems)
+
+changeUIWhenNoLi();
